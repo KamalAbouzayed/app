094893caba4116fb4c13bcac8ec46e2d04155c98
KamalAbouzayed <Kamal.Abouzayed@geexar.dev>
2025-04-07 15:03:11 +0200

    upadate

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -8,8 +8,7 @@ use Illuminate\Http\Request;
 
 class AuthController extends Controller
 {
-    public function login(Request $request)
-    {
+    public function login(Request $request){
         $request->validate([
             'email' => 'required|email',
             'password' => 'required',
